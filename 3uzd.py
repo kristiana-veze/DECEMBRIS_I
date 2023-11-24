17a91a32cdab7fcb7d3aa99b1246bc36612a9673
kristiana-veze <kristianaveze@gmail.com>
2023-11-24 14:25:35 +0200

    3.uzd

--- 3uzd.py
+++ 3uzd.py
@@ -2,7 +2,7 @@ sk = int(input("Ievadiet skaitli"))
 parbaude = 2
 
 if sk % parbaude == 0 or sk == 2:
-    print("Izvēlētais skaitlis ir pāra skaitlis")
+    print("Izvēlētais skaitlis ir pāra skaitlis.")
 
 else:
-    print("Izvēlētais skaitlis ir nepāra skaitlis") 
+    print("Izvēlētais skaitlis ir nepāra skaitlis.") 
