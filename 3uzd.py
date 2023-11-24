4d5dd9c20bc187c1736f9bc4812b4f44a3ed74ad
kristiana-veze <kristianaveze@gmail.com>
2023-11-24 14:00:39 +0200

    Iesniedzu 3.uzd

---
+++ 3uzd.py
@@ -0,0 +1,8 @@
+sk = int(input("Ievadiet skaitli"))
+parbaude = 2
+
+if sk % parbaude == 0 or sk == 2:
+    print("Izvēlētais skaitlis ir pāra skaitlis")
+
+else:
+    print("Izvēlētais skaitlis ir nepāra skaitlis")
---
+++ README.txt
@@ -0,0 +1,1 @@
+Kristiāna Vēze
