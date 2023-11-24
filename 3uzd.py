8a10b5caa271b80f4017de7a4907257ba7d75093
kristiana-veze <kristianaveze@gmail.com>
2023-11-24 14:01:21 +0200

    3.uzd

--- 3uzd.py
+++ 3uzd.py
@@ -5,4 +5,4 @@ if sk % parbaude == 0 or sk == 2:
     print("Izvēlētais skaitlis ir pāra skaitlis")
 
 else:
-    print("Izvēlētais skaitlis ir nepāra skaitlis")
+    print("Izvēlētais skaitlis ir nepāra skaitlis") 
